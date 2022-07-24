a0151926970f79af3d7fe374208129540c2ceb7f
Vicky Lu <vicky.lu@OVX1K92C6DR9MAC.attlocal.net>
2022-07-24 12:24:19 -0400

    flip animation complete

--- src/components/Numbers.js
+++ src/components/Numbers.js
@@ -5,12 +5,23 @@ const Numbers = (props) => {
     return (
         <div className="number-wrapper__individual_digit_main">
             <div className={props.currentNumber !== props.nextNumber ? 'numbers--group__wrapper updating' : 'numbers--group__wrapper'}>
-                <div className="numbers--top">
-                    <p>{props.currentNumber}</p>
-                </div>
                 <div className="numbers--bottom">
                     <p>{props.currentNumber}</p>
                 </div>
+
+                <div className="numbers--top">
+                    <p>{props.nextNumber}</p>
+                </div>
+                <div className="numbers--top">
+                    <p>{props.nextNumber}</p>
+                </div>
+                <div className="numbers--top flipper">
+                    <p>{props.currentNumber}</p>
+                </div>
+                <div className="numbers--top flipper nextValue">
+                    <p>{props.nextNumber}</p>
+                </div>
+
             </div>
         </div>
     );
